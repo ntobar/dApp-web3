4ffad67f06aa0a87d8a6f07bb9de8886c3c24833
nicolas tobar <ntobar@bluecedar.com>
2022-05-06 03:30:34 -0400

    added buttons

--- dapp3/src/components/boilerplate/boilerplate.tsx
+++ dapp3/src/components/boilerplate/boilerplate.tsx
@@ -5,6 +5,9 @@ function boilerplate() {
         <div>]
 
             <h1>This is my dApp</h1>
+            <h1>Here we can set or get the mood!</h1>
+            <button>Get Mood</button>
+            <button>Set Mood</button>
             
         </div>
     )
