dd5af7b6bbbc7120d520a7a3325307e6cfb109ff
nicolas tobar <ntobar@bluecedar.com>
2022-05-06 03:29:08 -0400

    added header

---
+++ dapp3/src/components/boilerplate/boilerplate.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function boilerplate() {
+    return (
+        <div>]
+
+            <h1>This is my dApp</h1>
+            
+        </div>
+    )
+}
+
+export default boilerplate
