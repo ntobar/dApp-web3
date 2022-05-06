4ff300c3d26984954e5312204c83fad4c143aae6
nicolas tobar <ntobar@bluecedar.com>
2022-05-06 03:31:59 -0400

    added basic styling:

--- dapp3/src/components/boilerplate/boilerplate.tsx
+++ dapp3/src/components/boilerplate/boilerplate.tsx
@@ -6,8 +6,9 @@ function boilerplate() {
 
             <h1>This is my dApp</h1>
             <h1>Here we can set or get the mood!</h1>
-            <button>Get Mood</button>
-            <button>Set Mood</button>
+            <label htmlFor="mood"></label>
+            <button onClick={}>Get Mood</button>
+            <button onClick={}>Set Mood</button>
             
         </div>
     )
