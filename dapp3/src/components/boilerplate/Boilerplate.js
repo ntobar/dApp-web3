e7f7b9a082f5aac1cd34457f370b513f958c1196
nicolas tobar <ntobar@bluecedar.com>
2022-05-06 03:54:45 -0400

    added server:

--- dapp3/src/components/boilerplate/Boilerplate.js
+++ dapp3/src/components/boilerplate/Boilerplate.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function boilerplate() {
+function Boilerplate() {
     return (
         <div>]
 
             <h1>This is my dApp</h1>
             <h1>Here we can set or get the mood!</h1>
             <label htmlFor="mood"></label>
-            <button onClick={}>Get Mood</button>
-            <button onClick={}>Set Mood</button>
+            <button>Get Mood</button>
+            <button>Set Mood</button>
             
         </div>
     )
 }
 
-export default boilerplate
+export default Boilerplate
